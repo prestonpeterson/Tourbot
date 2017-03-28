6d729bb39b7c28dc34c779d930bd7fbc980fe196
BryceSchmitt <darthscoutious@yahoo.com>
2017-03-28 16:14:50 -0700

    Adds class for ultrasonic sensors
    Untested

---
+++ HCRS04Ultrasonic/HCRS04Ultrasonic.cpp
@@ -0,0 +1,58 @@
+/*
+This class allows control of the HC-SR04 Ultrasonic sensor
+*/
+
+#include "HCSR04Ultrasonic.h"
+
+HCSR04Ultrasonic::HCSR04Ultrasonic(int triggerPinNum, int echoPinNum) {
+	triggerPin = triggerPinNum;
+	echoPin = echoPinNum;
+	voltagePin = -1;
+	groundPin = -1;
+	init();
+}
+
+HCSR04Ultrasonic::HCSR04Ultrasonic(int triggerPinNum, int echoPinNum, int voltPinNum, int groundPinNum){
+	triggerPin = triggerPinNum;
+	echoPin = echoPinNum;
+	voltagePin = voltPinNum;
+	groundPin = groundPinNum;
+	init();
+}
+
+int HCSR04Ultrasonic::getDistanceRaw(){
+	// The sensor is triggered by a HIGH pulse of 10 or more microseconds.
+	// Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+	digitalWrite(triggerPin, LOW);
+	delayMicroseconds(5);
+	digitalWrite(triggerPin, HIGH);
+	delayMicroseconds(10);
+	digitalWrite(triggerPin, LOW);
+
+	// Read the signal from the sensor: a HIGH pulse whose
+	// duration is the time (in microseconds) from the sending
+	// of the ping to the reception of its echo off of an object.
+	return pulseIn(echoPin, HIGH);
+}
+
+double HCSR04Ultrasonic::getDistanceCm(){
+	return (getDistanceRaw() / 2.0) / 29.1;
+	
+}
+
+double HCSR04Ultrasonic::getDistanceIn(){
+	return (getDistanceRaw() / 2.0) / 74;
+}
+
+void HCSR04Ultrasonic::init(){
+	pinMode(triggerPin, OUTPUT);
+	pinMode(echoPin, INPUT);
+	if (voltagePin > 0) {
+		pinMode(voltagePin, OUTPUT);
+		digitalWrite(voltagePin, HIGH);
+	}	
+	if (groundPin > 0) {
+		pinMode(groundPin, OUTPUT);
+		digitalWrite(groundPin, LOW);
+	}
+}
---
+++ HCRS04Ultrasonic/HCRS04Ultrasonic.h
@@ -0,0 +1,19 @@
+/*
+This class allows control of the HC-SR04 Ultrasonic sensor
+*/
+
+#ifndef HCSR04ULTRASONIC
+#define HCSR04ULTRASONIC
+
+class HCSR04Utrasonic{
+public:
+	HCSR04Ultrasonic(int triggerPinNum, int echoPinNum);
+	HCSR04Ultrasonic(int triggerPinNum, int echoPinNum, int voltPinNum, int groundPinNum);
+	int getDistanceRaw();
+	double getDistanceCm();
+	double getDistanceIn();
+private:
+	int voltagePin, groundPin, triggerPin, echoPin;
+
+	void init();
+};
